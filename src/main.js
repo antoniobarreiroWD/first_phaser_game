34c4f8865b72203e378c22e370b2f371f375422a
antoniobarreiroimmune <antonio.barreiro.dw@immune.institute>
2024-07-20 13:55:50 +0200

    ajustes de ventana y fondo

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 const config = {
     type: Phaser.AUTO,
-    width: window.innerWidth,
-    height: window.innerHeight,
-    parent: 'game-container',
+    width: window.innerWidth * 0.8, 
+    height: window.innerHeight * 0.8, 
+    parent: 'game',
     physics: {
         default: 'arcade',
         arcade: {
@@ -82,19 +82,15 @@ function create() {
 
     let enemyPositions = [
         { x: 600, y: 450 },
-        { x: 1200, y: 450 },
-        { x: 1800, y: 450 },
-        { x: 2400, y: 450 },
-        { x: 3000, y: 450 },
-        { x: 3600, y: 450 },
-        { x: 4200, y: 450 }
+        { x: 1500, y: 450 },
     ];
 
     enemyPositions.forEach((pos) => {
         let enemy = this.enemies.create(pos.x, pos.y, 'zombieWalk1').setScale(0.4);
         enemy.setBounce(0.2);
         enemy.setCollideWorldBounds(true);
-        enemy.setVelocityX(Phaser.Math.Between(-70, 70));
+        let velocity = Phaser.Math.Between(0, 1) === 0 ? 40 : -40;
+        enemy.setVelocityX(velocity);
     });
 
     this.physics.add.collider(this.enemies, this.platforms);
@@ -105,12 +101,8 @@ function create() {
         frames: [
             { key: 'zombieWalk1' },
             { key: 'zombieWalk2' },
-            { key: 'zombieWalk3' },
-            { key: 'zombieWalk4' },
-            { key: 'zombieWalk5' },
-            { key: 'zombieWalk6' }
         ],
-        frameRate: 10,
+        frameRate: 5,
         repeat: -1
     });
 
@@ -118,71 +110,53 @@ function create() {
         enemy.anims.play('enemyWalk', true);
     });
 
-    const leftButton = document.getElementById('left-button');
-    const rightButton = document.getElementById('right-button');
-    const jumpButton = document.getElementById('jump-button');
-
-    let moveLeft = false;
-    let moveRight = false;
-
-    leftButton.addEventListener('pointerdown', () => moveLeft = true);
-    leftButton.addEventListener('pointerup', () => moveLeft = false);
-
-    rightButton.addEventListener('pointerdown', () => moveRight = true);
-    rightButton.addEventListener('pointerup', () => moveRight = false);
-
-    jumpButton.addEventListener('pointerdown', () => {
-        if (this.player.body.touching.down) {
-            this.player.setVelocityY(-400);
-        }
-    });
-
-    this.input.addPointer(3);
-    this.moveLeft = moveLeft;
-    this.moveRight = moveRight;
-
-    leftButton.addEventListener('pointerdown', () => this.moveLeft = true);
-    leftButton.addEventListener('pointerup', () => this.moveLeft = false);
-
-    rightButton.addEventListener('pointerdown', () => this.moveRight = true);
-    rightButton.addEventListener('pointerup', () => this.moveRight = false);
-
     adjustCameraZoom();
 }
 
 function update() {
-    if (this.cursors.left.isDown || this.moveLeft) {
-        this.player.setVelocityX(-160);
-        this.player.anims.play('left', true);
-        this.player.setFlipX(true);
-    } else if (this.cursors.right.isDown || this.moveRight) {
-        this.player.setVelocityX(160);
-        this.player.anims.play('right', true);
-        this.player.setFlipX(false);
-    } else {
-        this.player.setVelocityX(0);
-        this.player.anims.play('turn');
-    }
-
-    if ((this.cursors.up.isDown || this.moveUp) && this.player.body.touching.down) {
-        this.player.setVelocityY(-400);
-    }
-
-    this.background.tilePositionX = this.cameras.main.scrollX * 0.5;
-
-    this.enemies.children.iterate(function (enemy) {
-        if (enemy.body.velocity.x < 0) {
-            enemy.setFlipX(true);
-        } else if (enemy.body.velocity.x > 0) {
-            enemy.setFlipX(false);
+    if (window.innerWidth >= 768) {
+        if (this.cursors.left.isDown) {
+            this.player.setVelocityX(-160);
+            this.player.anims.play('left', true);
+            this.player.setFlipX(true); 
+        } else if (this.cursors.right.isDown) {
+            this.player.setVelocityX(160);
+            this.player.anims.play('right', true);
+            this.player.setFlipX(false); 
+        } else {
+            this.player.setVelocityX(0);
+            this.player.anims.play('turn');
         }
 
+        if (this.cursors.up.isDown && this.player.body.touching.down) {
+            this.player.setVelocityY(-400);
+        }
+    }
+
+    
+    this.background.tilePositionX = this.cameras.main.scrollX * 0.5;
+
+    
+    const enemySpeed = 70; 
+    this.enemies.children.iterate(function (enemy) {
+        if (!enemy.body.velocity.x) {
+           
+            enemy.setVelocityX(Phaser.Math.Between(0, 1) ? enemySpeed : -enemySpeed);
+        }
+
+        
+        enemy.setFlipX(enemy.body.velocity.x < 0);
+
+        
         if (enemy.body.blocked.left) {
-            enemy.setVelocityX(70);
-            enemy.setFlipX(false);
+            enemy.setVelocityX(enemySpeed);
         } else if (enemy.body.blocked.right) {
-            enemy.setVelocityX(-70);
-            enemy.setFlipX(true);
+            enemy.setVelocityX(-enemySpeed);
+        }
+
+        
+        if (!enemy.anims.isPlaying) {
+            enemy.anims.play('enemyWalk', true);
         }
     });
 }
@@ -206,7 +180,8 @@ function adjustCameraZoom() {
 }
 
 window.addEventListener('resize', () => {
-    game.scale.resize(window.innerWidth, window.innerHeight);
-    game.cameras.resize(window.innerWidth, window.innerHeight);
+    game.scale.resize(window.innerWidth * 0.8, window.innerHeight * 0.8); 
     adjustCameraZoom();
 });
+
+window.game = game;
