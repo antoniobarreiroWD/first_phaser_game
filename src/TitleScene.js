5a4e49c8b7771058333af7e4e78f86aa28436110
Antonio Barreiro <antonio.barreiro.dw@immune.institute>
2024-08-01 19:33:17 +0200

    Update TitleScene.js

--- src/TitleScene.js
+++ src/TitleScene.js
@@ -7,9 +7,14 @@ class TitleScene extends Phaser.Scene {
         let titleStyle = { fontSize: '48px', fill: '#fff' };
         let buttonStyle = { fontSize: '32px', fill: '#fff' };
 
-        if (window.innerWidth < 800) {
+        if (window.innerWidth < 999) {
             titleStyle.fontSize = '32px';
             buttonStyle.fontSize = '24px';
+        }
+
+        if (window.innerWidth < 420) {
+            titleStyle.fontSize = '18px';
+            buttonStyle.fontSize = '16px';
         }
 
         this.add.text(this.cameras.main.centerX, this.cameras.main.centerY - 100, 'Las aventuras de Mairim', titleStyle).setOrigin(0.5);
